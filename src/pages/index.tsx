944f148ed5d7fa195f3a5ab764f3f1d0d5b5bfac
Khoerul Umam <khoerul.umam@gotoko.co.id>
2023-09-08 10:37:00 +0700

    up: smtp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import { Oxanium } from 'next/font/google'
 import { useState, type MouseEvent, type KeyboardEvent } from "react";
-import sendEmail from '@/services/email/send';
+import { sendEmailSMTP } from '@/services/email/send';
 
 const oxanium = Oxanium({ subsets: ['latin'] })
 
@@ -11,7 +11,7 @@ export default function Home() {
 
   const handleSubmitEmail = async () => {
     console.log(email, message);
-    await sendEmail(email, message);
+    void await sendEmailSMTP(email, message);
   }
 
   return (
@@ -40,7 +40,7 @@ export default function Home() {
             <input type="email" id="helper-text" aria-describedby="helper-text-explanation" className="mb-3 bg-blackgold border border-gold text-gold text-sm rounded-xl focus:ring-gold focus:border-gold block w-full p-2.5 " placeholder="Email" onKeyUp={(e) => setEmail((e.target as HTMLInputElement).value)}></input>
             <textarea id="message" rows={5} className="block p-2.5 w-full text-sm text-gray-900 bg-blackgold rounded-lg border border-gold focus:ring-gold focus:border-gold text-gold" placeholder="Message" onKeyUp={(e) => setMessage((e.target as HTMLInputElement).value)}></textarea>
             <button type='button' className='bg-gold text-white block w-full mt-3 py-2 rounded-xl hover:bg-darkgold focus:bg-lightgold' onClick={handleSubmitEmail}>Send</button>
-            <p className='text-gold mt-3 text-center'>In Collaboration With Azura Labs and Molka</p>
+            <p className='text-gold mt-3 text-center'>In Collaboration With Azura Labs and Molca</p>
           </div>
         </div>
       </div>
--- src/services/email/send.ts
+++ src/services/email/send.ts
@@ -1,4 +1,5 @@
 import sgMail from '@sendgrid/mail';
+import {SMTPClient} from 'smtp-client';
 sgMail.setApiKey(process.env.SENDGRID_ENV as string);
 
 interface SendEmailResponse {
@@ -23,4 +24,27 @@ const sendEmail = async (email: string, message: string): Promise<SendEmailRespo
   return result;
 }
 
-export default sendEmail
+const sendEmailSMTP = async (email: string, message: string) => {
+  const config = {
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: true,
+    auth: {
+      user: 'khoerul27@gmail.com',
+      pass: 'vdZm3wzY7nPjMpI1'
+    }
+  };
+
+  const smtp = new SMTPClient(config);
+  await smtp.connect();
+  await smtp.authPlain({ username: 'khoerul27@gmail.com', password: 'vdZm3wzY7nPjMpI1' });
+  await smtp.mail({ from: email });
+  await smtp.rcpt({ to: 'khoerul27@gmail.com' });
+  await smtp.data(message);
+  await smtp.close();
+}
+
+export {
+  sendEmail,
+  sendEmailSMTP
+}
