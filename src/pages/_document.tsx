84dc327ba189bdd6646912a49c7c72f36c1b4f1a
Khoerul Umam <khoerul.umam@gotoko.co.id>
2023-09-08 13:16:10 +0700

    up: favicon title and metadata

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,25 @@
+/* eslint-disable @next/next/no-title-in-document-head */
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head title='RMSynergy' />
+      <Head>
+        <meta property="og:title" content="RMTech" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://rmtech.id" />
+        <meta property="og:image" content="https://www.rmtech.id/og.png" />
+        <meta property="og:description" content="Generate solutions for various limitations" />
+
+        <meta name="twitter:title" content="RMTech" />
+        <meta name="twitter:description" content="Generate solutions for various limitations" />
+        <meta name="twitter:image" content="https://www.rmtech.id/og.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+              
+        <link rel="shortcut icon" href="/png/favicon.png" type="image/x-icon"></link>
+
+        <title>RMTech</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
